8cb8be28636605371cf7dc1a66fb26c7ff76ec39
Josiah Baldwin <jbcartz@gmail.com>
2016-05-16 15:29:19 -0700

    Add files via upload

---
+++ js/plugin.js
@@ -0,0 +1,6 @@
+// jQuery Plugin
+$('img').imageLens({
+    lensSize: 300,
+    borderColor: '#FFFF31',
+    imageSrc: 'https://raw.githubusercontent.com/JosiahBaldwin/RWDProject/gh-pages/img/12187819_1013131275374034_7874430223503911590_n.jpg',
+});
